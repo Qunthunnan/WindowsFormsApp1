9289b4192d0b05bbed33f825076a0bce63eb3558
YuriiTrofimenko <tyaa@ukr.net>
2020-11-23 22:41:04 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.helpButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // helpButton
+            // 
+            this.helpButton.BackColor = System.Drawing.SystemColors.Info;
+            this.helpButton.Location = new System.Drawing.Point(13, 13);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(75, 23);
+            this.helpButton.TabIndex = 0;
+            this.helpButton.Text = "Click Me!";
+            this.helpButton.UseVisualStyleBackColor = false;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.resultLabel.Location = new System.Drawing.Point(94, 13);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(67, 24);
+            this.resultLabel.TabIndex = 1;
+            this.resultLabel.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.helpButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button helpButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private TextBox userInputTextBox;
+        private Label timeLabel;
+        public Form1()
+        {
+            InitializeComponent();
+            this.timeLabel = new Label();
+            this.timeLabel.Location = new Point(this.Width / 2 - 33, this.Height / 2 - 12);
+            this.timeLabel.Size = new System.Drawing.Size(67, 24);
+            this.Controls.Add(timeLabel);
+            Timer timer = new Timer();
+            timer.Tick += (timerEventSender, timerEventArgs) => {
+                DateTime dateTimeNow = DateTime.Now;
+                int hours = dateTimeNow.Hour;
+                int minutes = dateTimeNow.Minute;
+                int seconds = dateTimeNow.Second;
+                this.timeLabel.Text = $"{hours}:{minutes}:{seconds}";
+            };
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+
+            // MessageBox.Show("Form1 App Help. ...");
+            // Console.WriteLine("Form1 App Console Help. ...");
+            // Console.WriteLine(((Button)sender).Name);
+            ((Button)sender).Enabled = false;
+            Timer timer = new Timer();
+            timer.Tick += (timerEventSender, timerEventArgs) => {
+                Console.WriteLine("((Button)sender).Enabled = true;");
+                ((Button)sender).Enabled = true;
+                timer.Dispose();
+            };
+            timer.Interval = 5000;
+            timer.Start();
+            this.resultLabel.Text = "Result: some text ...";
+            if (this.userInputTextBox == null)
+            {
+                this.userInputTextBox = new TextBox();
+                this.userInputTextBox.Size = new Size(150, 25);
+                this.userInputTextBox.Top = 42;
+                this.userInputTextBox.Left = 13;
+                this.userInputTextBox.TextChanged += (textChangedSender, textChangedEventArgs) => {
+                    this.resultLabel.Text = this.userInputTextBox.Text;
+                };
+                this.Controls.Add(this.userInputTextBox);
+            }
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            if (this.timeLabel != null)
+            {
+                this.timeLabel.Location = new Point(this.Width / 2 - 33, this.Height / 2 - 12);
+            }
+        }
+    }
+}
